87cce15e17fd477ea35dcbd3fafe09509ee6ac49
Vladimir Zhukov <vzh.inbox@gmail.com>
2017-09-03 15:17:18 +0600

    refactor: cleanup and optimize

--- client/src/Flex/Flex.tsx
+++ client/src/Flex/Flex.tsx
@@ -10,37 +10,43 @@ export type JustifyContent = Align | 'space-between' | 'space-around';
 export type AlignItems = Align | 'stretch' | 'baseline';
 export type AlignSelf = AlignItems | 'auto';
 export type AlignContent = JustifyContent | 'stretch';
+export type FlexBasis =
+  | 'none'
+  | 'auto'
+  | 'fill'
+  | 'content'
+  | 'fit-content'
+  | 'min-content'
+  | 'max-content';
+export type Fill = 'v' | 'h' | 'all';
 
 export interface Props
   extends React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> {
-  display?: 'flex' | 'inline-flex';
-  // flex?: string | number | boolean;
-  flexBasis?: 'none' | 'auto' | 'fill' | 'content' | 'fit-content' | 'min-content' | 'max-content';
-  /** Shortcut for `display="inline-flex"` */
+  /** Sets `display` to `inline-flex` */
   inline?: boolean;
-  /** Used with `row` or `col`. Sets `flow-direction` to `column-reverse` or `row-reverse` */
-  reverse?: boolean;
+  alignContent?: AlignContent;
+  alignItems?: AlignItems;
+  alignSelf?: AlignSelf;
+  justifyContent?: JustifyContent;
+  flexBasis?: FlexBasis;
   /** Sets `flow-direction` to `row`. */
   row?: boolean;
   /** Sets `flow-direction` to `column`. Takes a precedence over `row`. */
   column?: boolean;
+  /** Used with `row` or `col`. Sets `flow-direction` to `column-reverse` or `row-reverse` */
+  reverse?: boolean;
   /** Sets `flex-wrap` to `wrap`. */
   wrap?: boolean;
-  alignItems?: AlignItems;
-  alignSelf?: AlignSelf;
-  alignContent?: AlignContent;
-  justifyContent?: JustifyContent;
   /** Stretch by horizontal. */
   hfill?: boolean;
   /** Stretch by vertical. */
   vfill?: boolean;
   /** Stretch by v - vertical or h - horizontal or all - both. */
-  fill?: 'v' | 'h' | 'all';
+  fill?: Fill;
   /** Sets React component as a container. */
   component?: React.ComponentType<any>;
   /** Html tag name for output container. Takes a precedence over `component`. */
   tagName?: string;
-  className?: string;
 }
 
 export default function Flex(props: Props) {
@@ -55,26 +61,20 @@ export default function Flex(props: Props) {
 }
 
 function props2className(props: Props): string {
-  const reverse = props.reverse ? '-reverse' : '';
-  const display = props.inline ? 'inline-flex' : props.display;
   const column = !!props.column;
   const row = !column && !!props.row;
-  // const flexBasis = props.fill ? 'fill' : props.flexBasis;
-  const flexBasis = props.flexBasis;
+  const reverse = props.reverse ? '-reverse' : '';
 
   const className = classNames(
-    (display && css[`${PREFIX}display-${display}`]) || css[`${PREFIX}display-flex`],
-    props.alignItems && css[`${PREFIX}align-items-${props.alignItems}`],
+    props.inline ? css[`${PREFIX}display-inline-flex`] : css[`${PREFIX}display-flex`],
     props.alignContent && css[`${PREFIX}align-content-${props.alignContent}`],
-    props.justifyContent && css[`${PREFIX}justify-content-${props.justifyContent}`],
-    props.wrap && css[`${PREFIX}wrap`],
+    props.alignItems && css[`${PREFIX}align-items-${props.alignItems}`],
     props.alignSelf && css[`${PREFIX}align-self-${props.alignSelf}`],
+    props.justifyContent && css[`${PREFIX}justify-content-${props.justifyContent}`],
+    props.flexBasis && css[`${PREFIX}flex-basis-${props.flexBasis}`],
     row && css[`${PREFIX}row${reverse}`],
     column && css[`${PREFIX}column${reverse}`],
-    // props.flex != null && css[`${PREFIX}flex-${flex}`],
-    // props.flexGrow != null && css[`${PREFIX}flex-grow-${flexGrow}`],
-    // props.flexShrink != null && css[`${PREFIX}flex-shrink-${flexShrink}`],
-    flexBasis && css[`${PREFIX}flex-basis-${flexBasis}`],
+    props.wrap && css[`${PREFIX}wrap`],
     props.hfill && css[`${PREFIX}fill-h`],
     props.vfill && css[`${PREFIX}fill-v`],
     props.fill && css[`${PREFIX}fill-${props.fill}`],
--- client/src/Flex/utils.ts
+++ client/src/Flex/utils.ts
@@ -5,24 +5,21 @@ export interface Props {
 export function exclude(props: Props): Props {
   const result = { ...props };
 
-  delete result.display;
-  // delete result.flex;
-  delete result.flexBasis;
   delete result.inline;
-  delete result.reverse;
-  delete result.row;
-  delete result.column;
-  delete result.wrap;
+  delete result.alignContent;
   delete result.alignItems;
   delete result.alignSelf;
-  delete result.alignContent;
   delete result.justifyContent;
+  delete result.flexBasis;
+  delete result.row;
+  delete result.column;
+  delete result.reverse;
+  delete result.wrap;
   delete result.hfill;
   delete result.vfill;
   delete result.fill;
   delete result.component;
   delete result.tagName;
-  delete result.className;
 
   return result;
 }
