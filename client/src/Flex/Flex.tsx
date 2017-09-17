ebb68669f5159ba405e90d6ec21a2f9fa1743947
Vladimir Zhukov <vzh.inbox@gmail.com>
2017-09-17 22:45:30 +0600

    Add Props to export

--- client/src/Flex/Flex.tsx
+++ client/src/Flex/Flex.tsx
@@ -25,8 +25,7 @@ export type NumColumn = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 1
 export type NumStrColumn = '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '10' | '11' | '12' | '13' | '14' | '15' | '16' | '17 ' | '18' | '19' | '20' | '21' | '22' | '23' | '24';
 export type Column = NumColumn | NumStrColumn;
 
-export interface Props
-  extends React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> {
+export interface Props extends React.HTMLAttributes<HTMLElement> {
   /** Sets `display` to `inline-flex`. */
   inline?: boolean;
   /** Sets `align-content` to corresponding value. */
--- client/src/Flex/index.ts
+++ client/src/Flex/index.ts
@@ -1,1 +1,2 @@
 export { default } from './Flex';
+export * from './Flex';
