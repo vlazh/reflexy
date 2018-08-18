896ac6cca2fab18dfecb89dcd19268966b5fbe47
Vladimir Zhukov <vzh.inbox@gmail.com>
2018-08-18 21:57:58 +0600

    Update deps and refactor

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,3 @@
 const postcssConfig = require('@vzh/configs/css/postcssConfig').default;
-const baseConfig = postcssConfig();
 
-module.exports = {
-  ...baseConfig,
-  plugins: {
-    autoprefixer: baseConfig.plugins.autoprefixer,
-    cssnano: baseConfig.plugins.cssnano,
-  },
-};
+module.exports = postcssConfig();
--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,65 +1,34 @@
 import webpackMerge from 'webpack-merge';
-import path from 'path';
-import commonConfig from '@vzh/configs/webpack/common.config';
-import paths from '@vzh/configs/paths';
+import clientConfigAts from '@vzh/configs/webpack/client.config.ats';
+import { defaultRules } from '@vzh/configs/webpack/client.config';
 import loaders from '@vzh/configs/webpack/loaders';
 
 export default webpackMerge(
-  commonConfig({
-    outputPath: path.resolve(paths.root, 'dist'),
-    outputPublicPath: paths.client.output.publicPath,
+  clientConfigAts({
+    rhl: false,
+
+    entry: {
+      lib: ['./index'],
+    },
+
+    rules: {
+      cssRule: {
+        ...defaultRules.cssRule,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              regExp: 'src(?:/|\\\\)(.*)',
+              name: '[1]',
+            },
+          },
+          { loader: 'extract-loader' },
+          ...loaders.css({ pattern: '[local]', prodPattern: '[local]', sourceMap: false }),
+        ],
+      },
+    },
   }),
   {
-    context: paths.client.sources,
-
-    entry: { lib: './index' },
-
-    devtool: '',
-
-    output: {
-      filename: '[name].js',
-    },
-
-    resolve: {
-      modules: [paths.nodeModules.root, paths.client.sources, paths.root],
-    },
-
-    externals: {
-      react: 'react',
-    },
-
-    module: {
-      rules: [
-        {
-          test: /\.tsx?$/,
-          include: [paths.client.sources],
-          use: loaders.ats({ tsconfig: './tsconfig.webpack.json' }),
-        },
-        {
-          test: /\.css$/,
-          include: [paths.client.sources],
-          use: [
-            {
-              loader: 'file-loader',
-              options: {
-                regExp: 'src(?:/|\\\\)(.*)',
-                name: '[1]',
-              },
-            },
-            { loader: 'extract-loader' },
-            {
-              loader: 'css-loader',
-              options: {
-                modules: true,
-                sourceMap: false,
-                localIdentName: '[local]',
-                importLoaders: 1,
-              },
-            },
-            { loader: 'postcss-loader' },
-          ],
-        },
-      ],
-    },
+    externals: { react: 'react' },
   }
 );
