b90d3d24fefd1a9594a475ad173d63af2f60d342
Vladimir <vzh.inbox@gmail.com>
2019-01-15 17:04:31 +0600

    Rename breakpoints

--- lib/src/mediaQueries.ts
+++ lib/src/mediaQueries.ts
@@ -8,42 +8,42 @@ export enum BreakpointGroup {
 }
 
 export enum Breakpoint {
-  PhoneSm = 'phone-sm',
-  PhoneMd = 'phone-md',
-  PhoneLg = 'phone-lg',
-  TabletSm = 'tablet-sm',
-  TabletMd = 'tablet-md',
-  TabletLg = 'tablet-lg',
-  DesktopSm = 'desktop-sm',
-  DesktopMd = 'desktop-md',
-  DesktopLg = 'desktop-lg',
+  PhoneS = 'phone-s',
+  PhoneM = 'phone-m',
+  PhoneL = 'phone-l',
+  TabletS = 'tablet-s',
+  TabletM = 'tablet-m',
+  TabletL = 'tablet-l',
+  DesktopS = 'desktop-s',
+  DesktopM = 'desktop-m',
+  DesktopL = 'desktop-l',
 }
 
 export type Breakpoints = BreakpointGroup | Breakpoint;
 
 export const mediaQueries = Object.freeze({
   [BreakpointGroup.Phone]: {
-    [Breakpoint.PhoneSm]:
+    [Breakpoint.PhoneS]:
       'only screen and (max-device-width: 320px) and (orientation: portrait), only screen and (max-device-height: 320px) and (orientation: landscape)',
-    [Breakpoint.PhoneMd]:
+    [Breakpoint.PhoneM]:
       'only screen and (min-device-width: 321px) and (max-device-width: 414px) and (orientation: portrait), only screen and (min-device-height: 321px) and (max-device-height: 414px) and (orientation: landscape)',
-    [Breakpoint.PhoneLg]:
+    [Breakpoint.PhoneL]:
       'only screen and (min-device-width: 415px) and (max-device-width: 480px) and (orientation: portrait), only screen and (min-device-height: 415px) and (max-device-height: 480px) and (orientation: landscape)',
   },
   [BreakpointGroup.Tablet]: {
-    [Breakpoint.TabletSm]:
+    [Breakpoint.TabletS]:
       'only screen and (min-device-width: 481px) and (max-device-width: 600px) and (orientation: portrait), only screen and (min-device-height: 481px) and (max-device-height: 600px) and (orientation: landscape)',
-    [Breakpoint.TabletMd]:
+    [Breakpoint.TabletM]:
       'only screen and (min-device-width: 601px) and (max-device-width: 768px) and (orientation: portrait), only screen and (min-device-height: 601px) and (max-device-height: 768px) and (orientation: landscape)',
-    [Breakpoint.TabletLg]:
+    [Breakpoint.TabletL]:
       'only screen and (min-device-width: 769px) and (max-device-width: 1023px) and (orientation: portrait), only screen and (min-device-height: 769px) and (max-device-height: 1023px) and (orientation: landscape)',
   },
   [BreakpointGroup.Desktop]: {
-    [Breakpoint.DesktopSm]:
+    [Breakpoint.DesktopS]:
       'only screen and (min-device-width: 1024px) and (max-device-width: 1024px) and (orientation: portrait), only screen and (min-device-height: 1024px) and (max-device-height: 1024px) and (orientation: landscape)',
-    [Breakpoint.DesktopMd]:
+    [Breakpoint.DesktopM]:
       'only screen and (min-device-width: 1025px) and (max-device-width: 1919px) and (orientation: portrait), only screen and (min-device-height: 1025px) and (max-device-height: 1919px) and (orientation: landscape)',
-    [Breakpoint.DesktopLg]:
+    [Breakpoint.DesktopL]:
       'only screen and (min-device-width: 1920px) and (orientation: portrait), only screen and (min-device-height: 1920px) and (orientation: landscape)',
   },
 });
