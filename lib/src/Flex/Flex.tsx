005cf907f8f8ecff73e9d146b4917de754027dde
Vladimir <vzh.inbox@gmail.com>
2019-03-01 11:30:29 +0600

    Add filling by percentage and refactor

--- lib/src/Flex/Flex.tsx
+++ lib/src/Flex/Flex.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { ContentDistribution } from 'csstype';
 import classNames from 'classnames';
 import css from './Flex.css';
@@ -17,8 +17,6 @@ export type AlignContent = Globals | ContentDistribution | FlexPosition;
 export type FlexBasis = Globals | 'auto' | 'content';
 
 export type FlexWrap = Globals | 'nowrap' | 'wrap' | 'wrap-reverse';
-
-export type Fill = 'v' | 'h' | 'all' | boolean;
 
 // prettier-ignore
 type ColumnID = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 | 16 | 17  | 18 | 19 | 20 | 21 | 22 | 23 | 24;
@@ -63,12 +61,12 @@ export interface FlexProps extends Styleable {
   shrink?: Column;
   /** Sets `order` to corresponding value. */
   order?: number;
-  /** Stretch by horizontal. */
-  hfill?: boolean;
-  /** Stretch by vertical. */
-  vfill?: boolean;
-  /** Stretch by v - vertical or h - horizontal or all - both. Also accepts boolean value: `true` is equals to `all`. */
-  fill?: Fill;
+  /** Stretch by horizontal or sets width in percentage (numbers in range 0.0 to 1.0 inclusive). */
+  hfill?: boolean | number;
+  /** Stretch by vertical or sets height in percentage (numbers in range 0.0 to 1.0 inclusive). */
+  vfill?: boolean | number;
+  /** Stretch by vertical and horizontal. */
+  fill?: boolean;
 }
 
 export interface Childrenable {
@@ -101,13 +99,17 @@ export type FlexAllProps = FlexProps & FlexAdditionalProps & Childrenable;
  * Default style is just `display: flex;`.
  * Example: `<Flex component={<button />} ... />`
  */
-// React.ReactElement<any, string | ((props: any) => React.ReactElement<any, string | any | (new (props: any) => React.Component<any, any, any>)> | null) | (new (props: any) => React.Component<any, any, any>)>;
 export default function Flex(
   props: FlexAllProps
 ): React.ReactElement<(Styleable & Childrenable) | React.HTMLAttributes<HTMLDivElement>> {
-  const nextProps = omitFlexProps(props);
-  nextProps.className = props2className(props);
-  nextProps.style = props2style(props);
+  const nextProps = useMemo<Styleable & Childrenable>(
+    () => ({
+      className: props2className(props),
+      style: props2style(props),
+      children: props.children,
+    }),
+    [props]
+  );
 
   // render div
   if (!props.component) {
@@ -118,7 +120,7 @@ export default function Flex(
   // render custom component with flex props
   const component = React.Children.only(props.component);
   const componentProps: typeof nextProps = {
-    ...nextProps, // copy all data-* attrs and other
+    ...nextProps,
     className: classNames(nextProps.className, component.props.className),
     style: { ...component.props.style, ...nextProps.style },
   };
@@ -127,33 +129,6 @@ export default function Flex(
     return React.cloneElement(component, componentProps);
   }
   return React.cloneElement(component, componentProps, component.props.children, props.children);
-}
-
-export function omitFlexProps(props: FlexAllProps): Styleable & Childrenable {
-  const {
-    inline,
-    alignContent,
-    alignItems,
-    alignSelf,
-    justifyContent,
-    center,
-    basis,
-    grow,
-    shrink,
-    row,
-    column,
-    reverse,
-    wrap,
-    order,
-    hfill,
-    vfill,
-    fill,
-    component,
-    componentRef,
-    ...rest
-  } = props;
-
-  return rest;
 }
 
 export function props2className(props: FlexProps): string {
@@ -168,7 +143,9 @@ export function props2className(props: FlexProps): string {
   const wrap = (props.wrap === false && 'nowrap') || (props.wrap === true && 'wrap') || props.wrap;
   const alignItems = props.alignItems || (props.center && 'center');
   const justifyContent = props.justifyContent || (props.center && 'center');
-  const fill = props.fill === true ? 'all' : props.fill;
+  const fill = typeof props.fill === 'boolean' ? props.fill : undefined;
+  const hfill = props.hfill == null ? fill : typeof props.hfill === 'boolean' && props.hfill;
+  const vfill = props.vfill == null ? fill : typeof props.vfill === 'boolean' && props.vfill;
 
   const className = classNames(
     css[`display--${props.inline ? 'inline-flex' : 'flex'}`],
@@ -182,25 +159,28 @@ export function props2className(props: FlexProps): string {
     props.basis && css[`flex-basis--${props.basis}`],
     grow && css[`flex-grow--${grow}`],
     shrink && css[`flex-shrink--${shrink}`],
-    props.hfill && css['fill-h'],
-    props.vfill && css['fill-v'],
-    fill && css[`fill-${fill}`],
+    hfill && css['fill-h'],
+    vfill && css['fill-v'],
     props.className
   );
 
   return className;
 }
 
-export function props2style(props: FlexProps): React.CSSProperties | undefined {
-  const { style, order } = props;
+export function props2style({
+  style,
+  order,
+  hfill,
+  vfill,
+}: FlexProps): React.CSSProperties | undefined {
+  const width =
+    hfill != null && typeof hfill === 'number' ? `${Math.min(hfill, 1) * 100}%` : undefined;
+  const height =
+    vfill != null && typeof vfill === 'number' ? `${Math.min(vfill, 1) * 100}%` : undefined;
 
-  if (style == null && order == null) {
-    return undefined;
-  }
-
-  if (order == null) {
+  if (order == null && width == null && height == null) {
     return style;
   }
 
-  return { ...style, order };
+  return { ...style, order, width, height };
 }
