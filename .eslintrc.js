284ce24c392c2a8fb4d645b1e746b96cefff6790
Vladimir <vzh.inbox@gmail.com>
2019-10-05 21:15:36 +0600

    Update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
   extends: require.resolve('@vzh/configs/eslint/ts.react.eslintrc.js'),
   rules: {
     '@typescript-eslint/no-use-before-define': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
     'import/export': 'off',
   }
 };
