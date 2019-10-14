531e388ff281c87eb1c2491ff4d4ac5adfc96e72
Vladimir <vzh.inbox@gmail.com>
2019-10-14 22:23:10 +0600

    Refactor

--- src/FlexWithRef.tsx
+++ src/FlexWithRef.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Flex, { FlexAllProps, TweakableComponentType, DefaultComponentType } from './Flex';
 
+type RefType<P extends { [P: string]: any }> = 'ref' extends keyof P ? P['ref'] : never;
+
 const FlexWithRef = React.forwardRef(
   (props: FlexAllProps<DefaultComponentType>, ref: React.Ref<any>) => {
     return <Flex {...props} componentRef={ref} />;
@@ -8,11 +10,7 @@ const FlexWithRef = React.forwardRef(
 ) as <C extends TweakableComponentType = DefaultComponentType>(
   props: Omit<FlexAllProps<C>, 'component' | 'componentRef'> & {
     component: C;
-    ref?: undefined extends C
-      ? unknown
-      : ('ref' extends keyof React.ComponentPropsWithRef<C>
-          ? React.ComponentPropsWithRef<C>['ref']
-          : never);
+    ref?: undefined extends C ? unknown : RefType<React.ComponentPropsWithRef<C>>;
   }
 ) => JSX.Element;
 
