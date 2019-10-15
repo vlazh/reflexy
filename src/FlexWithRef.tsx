caba0cda9b17b0b5ee4e51be24256792222ba981
Vladimir <vzh.inbox@gmail.com>
2019-10-15 12:38:41 +0600

    Improve ts performance

--- src/FlexWithRef.tsx
+++ src/FlexWithRef.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import Flex, { FlexAllProps, TweakableComponentType, DefaultComponentType } from './Flex';
 
-type RefType<P extends { [P: string]: any }> = 'ref' extends keyof P ? P['ref'] : never;
+type PropsWithRef<P extends { [P: string]: any }> = P &
+  (P extends { componentRef?: any } ? { ref?: P['componentRef'] } : never);
 
 const FlexWithRef = React.forwardRef(
   (props: FlexAllProps<DefaultComponentType>, ref: React.Ref<any>) => {
     return <Flex {...props} componentRef={ref} />;
   }
 ) as <C extends TweakableComponentType = DefaultComponentType>(
-  props: Omit<FlexAllProps<C>, 'component' | 'componentRef'> & {
-    component: C;
-    ref?: undefined extends C ? unknown : RefType<React.ComponentPropsWithRef<C>>;
-  }
+  props: PropsWithRef<FlexAllProps<C> & { component: C }>
 ) => JSX.Element;
 
 export default FlexWithRef;
