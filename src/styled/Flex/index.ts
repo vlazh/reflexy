2152a2f6c842b8819374592a40d2a3ad1a1088c0
Vladimir <vzh.inbox@gmail.com>
2020-09-15 13:37:49 +0600

    Add exports

--- src/styled/Flex/index.ts
+++ src/styled/Flex/index.ts
@@ -1,2 +1,13 @@
 export { default } from './Flex';
 export * from './Flex';
+export type {
+  DefaultComponentType,
+  FlexComponentProps,
+  FlexAllProps,
+  Styleable,
+  DefaultSpaceSize,
+  OverflowProps,
+  FlexProps,
+  SpaceProps,
+  SpaceUnit,
+} from '../../Flex';
