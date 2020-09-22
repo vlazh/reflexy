d0fced3ed3efb1429e82b8a075f6ba08eb046b1c
Vladimir <vzh.inbox@gmail.com>
2020-09-22 13:11:15 +0600

    Add getViewSize

--- src/responsive/MediaQueries.ts
+++ src/responsive/MediaQueries.ts
@@ -30,6 +30,25 @@ export interface ViewSizeValue {
   maxWidth: number;
 }
 
+/** All values are unique. */
+export const viewSizeValues: Record<ViewSize, ViewSizeValue> = {
+  [ViewSize.xxs]: { minWidth: 0, maxWidth: 479 },
+  [ViewSize.xs]: { minWidth: 480, maxWidth: 767 },
+  [ViewSize.s]: { minWidth: 768, maxWidth: 991 },
+  [ViewSize.m]: { minWidth: 992, maxWidth: 1279 },
+  [ViewSize.l]: { minWidth: 1280, maxWidth: 1919 },
+  [ViewSize.xl]: { minWidth: 1920, maxWidth: 2559 },
+  [ViewSize.xxl]: { minWidth: 2560, maxWidth: Number.MAX_SAFE_INTEGER },
+};
+
+export function getViewSize(width: number): ViewSize {
+  const [viewSize = ViewSize.xxl] =
+    MediaQueries.viewSizeValueList.find(
+      ([, value]) => width >= value.minWidth && width <= value.maxWidth
+    ) ?? [];
+  return viewSize;
+}
+
 export interface MediaQueryEvent extends Pick<MediaQueryListEvent, 'matches'> {
   readonly viewSize: ViewSize;
 }
@@ -42,15 +61,7 @@ export interface MediaQueriesInitOptions {
 
 export default abstract class MediaQueries {
   /** All values are unique. */
-  static readonly viewSizeValues: Record<ViewSize, ViewSizeValue> = {
-    [ViewSize.xxs]: { minWidth: 0, maxWidth: 479 },
-    [ViewSize.xs]: { minWidth: 480, maxWidth: 767 },
-    [ViewSize.s]: { minWidth: 768, maxWidth: 991 },
-    [ViewSize.m]: { minWidth: 992, maxWidth: 1279 },
-    [ViewSize.l]: { minWidth: 1280, maxWidth: 1919 },
-    [ViewSize.xl]: { minWidth: 1920, maxWidth: 2559 },
-    [ViewSize.xxl]: { minWidth: 2560, maxWidth: Number.MAX_SAFE_INTEGER },
-  };
+  static readonly viewSizeValues = viewSizeValues;
 
   private static deviceDimentions = false;
   private static viewSizeValueListLazy: readonly [ViewSize, ViewSizeValue][] | undefined;
