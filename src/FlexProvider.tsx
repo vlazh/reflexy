c4433a7be6aa74c450a53adb19c1bca0e192e98b
Vladimir <vzh.inbox@gmail.com>
2022-10-28 17:01:24 +0600

    Add default values

--- src/FlexProvider.tsx
+++ src/FlexProvider.tsx
@@ -1,6 +1,6 @@
 import React, { useMemo } from 'react';
 import ThemeProvider from '@mui/styles/ThemeProvider';
-import type { SharedDefaults } from './sharedDefaults';
+import sharedDefaults, { SharedDefaults } from './sharedDefaults';
 import type { Theme } from './styled';
 
 export type FlexProviderProps = Partial<SharedDefaults>;
@@ -8,13 +8,13 @@ export type FlexProviderProps = Partial<SharedDefaults>;
 export const FlexContext = React.createContext<FlexProviderProps>({});
 
 export default function FlexProvider({
-  defaultUnit,
-  defaultSize,
-  defaultSizes,
+  defaultUnit = sharedDefaults.defaultUnit,
+  defaultSize = sharedDefaults.defaultSize,
+  defaultSizes = sharedDefaults.defaultSizes,
   ...rest
 }: React.PropsWithChildren<FlexProviderProps>): JSX.Element {
   const [defaults, theme] = useMemo<[FlexProviderProps, Theme]>(() => {
-    const values = Object.freeze<FlexProviderProps>({ defaultUnit, defaultSize, defaultSizes });
+    const values = Object.freeze<SharedDefaults>({ defaultUnit, defaultSize, defaultSizes });
     const t = Object.freeze<Theme>({ reflexy: values });
     return [values, t];
   }, [defaultSize, defaultSizes, defaultUnit]);
