9d3c679593cd4e8fb7d46d728d5a67b83bcc2c89
Vladimir <vzh.inbox@gmail.com>
2023-06-28 18:38:47 +0600

    Improve StyledReflexyJssWebpackPlugin

--- src/styled/StyledReflexyJssWebpackPlugin.ts
+++ src/styled/StyledReflexyJssWebpackPlugin.ts
@@ -1,17 +1,19 @@
 /* eslint-disable no-param-reassign */
 import path from 'path';
-import { NormalModuleReplacementPlugin } from 'webpack';
+import { NormalModuleReplacementPlugin, ResolveData } from 'webpack';
 
 /** Replaces regular imports of `Flex` with `styled/Flex` */
 export default class StyledReflexyJssWebpackPlugin extends NormalModuleReplacementPlugin {
-  constructor() {
+  constructor(filter?: (resource: ResolveData) => boolean | undefined) {
     // Works until webpack v5.49.0
     // super(/reflexy\/Flex\/Flex\.js/, '../styled/Flex/Flex.js');
 
     // Workaround for webpack v5.49.0+: https://github.com/webpack/webpack/issues/13957
     const resourceRegex = /reflexy\/Flex\/Flex\.js|reflexy\/styled\/Flex\/Flex\.js/;
     super(resourceRegex, (resource) => {
-      if (resource.createData) {
+      if (!resource.createData) return;
+      const filtered = filter && filter(resource);
+      if (filtered == null || filtered) {
         const resolvedRequest = resource.createData.userRequest as string;
         resource.createData.resource = resolvedRequest.replace(
           resourceRegex,
