413ee26dc3d2e37c5f9541be536fcda133f712e8
Vladimir <vzh.inbox@gmail.com>
2023-07-03 18:52:08 +0600

    Improve StyledReflexyWebpackPlugin

--- src/styled/StyledReflexyWebpackPlugin.ts
+++ src/styled/StyledReflexyWebpackPlugin.ts
@@ -4,15 +4,14 @@ import { NormalModuleReplacementPlugin } from 'webpack';
 
 /** Replaces regular imports of `Flex` with `styled/Flex` */
 export default class StyledReflexyWebpackPlugin extends NormalModuleReplacementPlugin {
-  constructor() {
+  constructor(resourceRegex = /reflexy\/Flex\/Flex\.js/) {
     // Works until webpack v5.49.0
     // super(/reflexy\/Flex\/Flex\.js/, '../styled/Flex/Flex.js');
 
     // Workaround for webpack v5.49.0+: https://github.com/webpack/webpack/issues/13957
-    const resourceRegex = /reflexy\/Flex\/Flex\.js/;
     super(resourceRegex, (resource) => {
-      if (resource.createData) {
-        const resolvedRequest = resource.createData.userRequest as string;
+      if (resource.createData && resource.createData.userRequest) {
+        const resolvedRequest = resource.createData.userRequest;
         resource.createData.resource = resolvedRequest.replace(
           resourceRegex,
           'reflexy/styled/Flex/Flex.js'
