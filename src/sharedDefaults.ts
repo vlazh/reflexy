43b19c7f6021443effc979bf01b2ff2ea0c8220f
Vladimir <vzh.inbox@gmail.com>
2025-08-27 17:17:11 +0500

    Update jsdocs

--- src/sharedDefaults.ts
+++ src/sharedDefaults.ts
@@ -1,19 +1,26 @@
 import type { SpaceSize, SpaceUnit } from './Flex';
 
 const sharedDefaults = {
-  /** Default measure of space */
+  /** Default measure of space. Defaults to `rem`. */
   defaultUnit: 'rem' as SpaceUnit,
 
-  /** Predefined default space sizes */
+  /** Predefined default space sizes. */
   defaultSizes: {
+    /** Defaults to `0.25`. */
     xs: 0.25,
+    /** Defaults to `0.5`. */
     s: 0.5,
+    /** Defaults to `1`. */
     m: 1,
+    /** Defaults to `1.5`. */
     l: 1.5,
+    /** Defaults to `2`. */
     xl: 2,
+    /** Defaults to `2.5`. */
     xxl: 2.5,
-  } as Record<SpaceSize, number>,
+  } satisfies Record<SpaceSize, number>,
 
+  /** Defaults to `m`. */
   defaultSize: 'm' as SpaceSize,
 };
 
